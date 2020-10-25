42033e321d6dcdc5528300bd943c2a6734b12c6e
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2020-10-24 21:23:03 -0400

    Initial Commit

---
+++ Shared/BuildNetflixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixAppApp.swift
+//  Shared
+//
+//  Created by Eugenia Molodetskaya on 2020-10-24.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
